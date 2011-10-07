e7158d0e1c3ad5a7104c4201c556c5697c1a8e0b
Jeffrey Basurto <jeffreybasurto@gmail.com>
2011-10-07 17:32:56 -0500

    added routes

--- app/app.rb
+++ app/app.rb
@@ -6,6 +6,15 @@ class Social < Padrino::Application
 
   enable :sessions
 
+
+  get "/" do
+    "Hello World!"
+  end
+
+  get :about, :map => '/about_us' do
+    render :haml, "%p This is a sample blog created to demonstrate how Padrino works!"
+  end
+
   ##
   # Caching support
   #
